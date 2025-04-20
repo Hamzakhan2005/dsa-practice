7b7e7e0f27375ad5c5a4fe57fa711c68654abf08
Mohammad Hamza Khan <mohdhamzakhan2005@gmail.com>
2025-04-20 17:53:54 +0530

    Added stock program

---
+++ arrays/Stocks.java
@@ -0,0 +1,27 @@
+import java.math.*;
+import java.util.*;
+
+public class Stocks {
+     public static void main(String[] args) {
+          int prices[]={7, 6, 4,  3, 1};
+          
+          System.out.println(buyAndSellStocks(prices));
+     }
+     public static int buyAndSellStocks(int prices[]){
+          int buyPrice=Integer.MAX_VALUE;
+          int maxProfit=0;
+
+          for(int i=0;i<prices.length;i++){
+               
+               if(buyPrice < prices[i]){
+                    int profit=prices[i]-buyPrice;
+                    maxProfit=Math.max(maxProfit,profit);
+               }else{
+                    
+                    buyPrice=prices[i];
+               }
+          }
+
+          return maxProfit;
+     }
+}
