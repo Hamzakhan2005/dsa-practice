f7cd101e355bd7086e77bb97aa5dfa2063840805
Mohammad Hamza Khan <mohdhamzakhan2005@gmail.com>
2025-05-14 15:02:44 +0530

    Added backtracking code

---
+++ backtracking/Array.java
@@ -0,0 +1,21 @@
+public class Array{
+     public static void main(String[] args) {
+          int arr[]=new int[5];
+          changeArray(arr, 0, 1);
+          printArray(arr);
+     }
+     public static void printArray(int arr[]){
+          for(int i=0;i<arr.length;i++){
+               System.out.print(arr[i]+" ");
+          }
+     }
+     public static void changeArray(int arr[],int i,int val){
+          if(i==arr.length){
+               printArray(arr);
+               return;
+          }
+          arr[i]=val;
+          changeArray(arr, i+1, val+1);
+          arr[i]-=2;//backtracking step
+     }
+}
