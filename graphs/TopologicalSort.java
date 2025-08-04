39c17781ea490c2870503a1d5ad2c2e9f0328b94
Mohammad Hamza Khan <mohdhamzakhan2005@gmail.com>
2025-08-04 21:58:53 +0530

    graph code

---
+++ graphs/TopologicalSort.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class TopologicalSort {
+     static class Edge{
+          int src;
+          int dest;
+          int wt;
+          public Edge(int s,int d,int w){
+               src=s;
+               dest=d;
+               wt=w;
+          }
+     }
+     public static void createGraph(ArrayList<Edge>[] graph){
+          
+          
+          for(int i=0;i<graph.length;i++){
+               graph[i]=new ArrayList<>();
+          }
+
+          
+
+          graph[2].add(new Edge(2, 3, 1));
+          
+          
+          graph[3].add(new Edge(3, 1, 1));
+          
+
+          graph[4].add(new Edge(4,0, 1));
+          graph[4].add(new Edge(4,1, 1));
+
+          graph[5].add(new Edge(5,0, 1));
+          graph[5].add(new Edge(5,2, 1));
+
+     }
+     public static void topoSort(ArrayList<Edge>[] graph){
+          boolean vis[]=new boolean[graph.length];
+          Stack<Integer> s=new Stack<>();
+
+          for(int i=0;i<graph.length;i++){
+               if(!vis[i]){
+                    topoSortUtil(graph,i,vis,s);
+               }
+          }
+          while (!s.isEmpty()) {
+               System.out.print(s.pop()+" ");
+          }
+     }
+     public static void topoSortUtil(ArrayList<Edge>[] graph,int curr,boolean vis[],Stack<Integer> s){
+          vis[curr]=true;
+          
+               for(int i=0;i<graph[curr].size();i++){
+                    Edge e=graph[curr].get(i);
+                    if (!vis[e.dest]) {
+                         topoSortUtil(graph, e.dest, vis, s);
+                    }
+               }
+          
+          
+          s.push(curr);
+     }
+     public static void main(String[] args) {
+          int V=6;
+          ArrayList<Edge>[] graph=new ArrayList[V];
+          createGraph(graph);
+          topoSort(graph);
+     }
+}
