69ead8380c5acdfb72f3aba076187dfdccd354b4
Mohammad Hamza Khan <mohdhamzakhan2005@gmail.com>
2025-08-10 20:35:43 +0530

    graph code

---
+++ greedy/FractionalKnapsack.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class FractionalKnapsack {
+     public static void main(String[] args) {
+          int val[]={60,100,120};
+          int wt[]={10,20,30};
+          int W=50;
+
+          double ratio[][]=new double[val.length][2];
+          for(int i=0;i<val.length;i++){
+               ratio[i][0]=i;
+               ratio[i][1]=val[i]/(double)wt[i];
+          }
+          // Ascending order
+          Arrays.sort(ratio,Comparator.comparingDouble(o->o[1]));
+          int capacity=W;
+          int value=0;
+
+          for(int i=ratio.length-1;i>=0;i--){
+               int idx=(int)ratio[i][0];
+               if(capacity>=wt[idx]){
+                    capacity-=wt[idx];
+                    value+=val[idx];
+               }else{
+                    value+=(ratio[i][1]*capacity);
+                    capacity=0;
+                    break;
+               }
+          }
+          System.out.println(value);
+     }
+}
