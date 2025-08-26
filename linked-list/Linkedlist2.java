581d8537e6e3cd04c4b0289a38b4618429c66a3e
Mohammad Hamza Khan <mohdhamzakhan2005@gmail.com>
2025-08-26 22:06:31 +0530

    linked-list code

---
+++ linked-list/Linkedlist2.java
@@ -0,0 +1,77 @@
+public class Linkedlist2 {
+     public static class Node{
+          int data;
+          Node next;
+          public Node(int data){
+               this.data=data;
+               this.next=null;
+          }
+     }
+     public static Node head;
+     public static Node tail;
+     public static int size;
+     public static void addFirst(int data){
+          Node newNode=new Node(data);
+          size++;
+          if(head==null){
+               head=tail=newNode;
+               return;
+          }
+          newNode.next=head;
+          head=newNode;
+     }
+     public boolean isCycle(){
+          Node slow=head;
+          Node fast=head;
+          while(fast!=null && fast.next!=null){
+               slow=slow.next;
+               fast=fast.next.next;
+               if(slow==fast){
+                    return true;
+               }
+          }
+          return false;
+     }
+     public void removCycle(){
+          Node slow=head;
+          Node fast=head;boolean exists=false;
+          while(fast!=null && fast.next!=null){
+               slow=slow.next;
+               fast=fast.next.next;
+               if(slow==fast){
+                    exists=true;
+                    break;
+               }
+          }
+          if(exists){
+               slow=head;
+               Node prev=null;
+               while (slow!=fast) {
+                    slow=slow.next;
+                    prev=fast;
+                    fast=fast.next;
+               }
+               prev.next=null;
+          }else{
+               return;
+          }
+     }
+     public static void main(String[] args) {
+          Linkedlist2 ll=new Linkedlist2();
+          head=new Node(1);
+          head.next=new Node(2);
+          head.next.next=new Node(3);
+          head.next.next.next=head.next;
+          System.out.println(ll.isCycle());
+          ll.removCycle();
+          System.out.println(ll.isCycle());
+     }
+     public void print (){
+          Node temp=head;
+          while(temp!=null){
+               System.out.print(temp.data+"->");
+               temp=temp.next;
+          }
+          System.out.println();
+     }
+}
